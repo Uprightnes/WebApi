8a8f9ad3c4a326f22f24b80136866e5b9bf2c227
Uprightnes <102225827+Uprightnes@users.noreply.github.com>
2024-03-27 14:31:19 +0100

    Add files via upload

---
+++ WebApiDemo/WebApiDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.Filters;
+using WebApiDemo.Filters.ActionFilters;
+using WebApiDemo.Models;
+using WebApiDemo.Models.Repositories;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetShirt()
+        {
+            return Ok(ShirtRepository.GetShirts());
+        }
+        [HttpGet("{id}")]
+        [Shirt_ValidateShirtIdFilter]
+        public IActionResult GetShirtById(int id)
+        {
+            return Ok(ShirtRepository.GetShirtById(id));
+            
+        }
+
+        [HttpPost]
+        [Shirt_ValidateCreateShirtFilter]
+        public IActionResult CreateShirt([FromBody]Shirt shirt)
+        {
+            
+            ShirtRepository.AddShirt(shirt);
+
+            return CreatedAtAction(nameof(GetShirtById),
+                new{id = shirt.ShirtId}, shirt);
+         
+        }
+
+        [HttpPut("{id}")]
+        [Shirt_ValidateCreateShirtFilter]
+        [Shirt_ValidateUpdateShirtFilter]
+        public IActionResult UpdateShirt(int id, Shirt shirt)
+        {    
+            ShirtRepository.UpdateShirt(shirt);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteShirt(int id)
+        {
+            var shirt = ShirtRepository.GetShirtById(id);
+            ShirtRepository.DeleteShirt(id);
+
+            return Ok(shirt);
+
+        }
+
+
+
+    }
+}
